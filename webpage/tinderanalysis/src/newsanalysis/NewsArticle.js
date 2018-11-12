32a54da9cac168c29c1f6d4eee28d1bf100e9422
estebandalelr <estebandalelr@gmail.com>
2018-11-12 13:31:37 -0500

    Show all news

---
+++ webpage/tinderanalysis/src/newsanalysis/NewsArticle.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+
+class NewsArticle extends Component {
+    state = {}
+    render() {
+        console.log(this.props)
+        return (
+            <div className="newsArticle">
+                <a href={this.props.Article.Link}>
+                    <h3>
+                        {this.props.Article.Title}
+                    </h3>
+                </a>
+                <p>
+                     Fecha: {this.props.Article.Date} Puntos: {this.props.Article.Points}  Comentarios: {this.props.Article.Comments}
+                </p>
+            </div>
+        );
+    }
+}
+
+export default NewsArticle;
--- webpage/tinderanalysis/src/newsanalysis/NewsHolder.js
+++ webpage/tinderanalysis/src/newsanalysis/NewsHolder.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 
+import NewsArticle from './NewsArticle';
+
 import "./NewsHolder.css";
+import allNews from "./news.json";
 
 class NewsHolder extends Component {
-    state = { news:[], }
+    parseNews(){
+        return allNews.map(article=> {
+           return <NewsArticle Article={article}/>                      
+        })
+    }
+
     render() { 
         return ( 
             <div>
                 NEWS
+                {this.parseNews()}
+                <h6>
+                    Tomado de <a href="https://news.ycombinator.com/">Hacker News</a>
+                </h6>
             </div>
          );
     }
