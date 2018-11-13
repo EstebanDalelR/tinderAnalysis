993cf6db3d11793b2e242d4c51b49a7c2aae3fbc
estebandalelr <estebandalelr@gmail.com>
2018-11-13 18:26:38 -0500

    Moving sections

--- webpage/tinderanalysis/src/newsanalysis/NewsHolder.js
+++ webpage/tinderanalysis/src/newsanalysis/NewsHolder.js
@@ -14,7 +14,7 @@ class NewsHolder extends Component {
 
     render() { 
         return ( 
-            <div>
+            <div className="News">
                 <h2 className="SectionTitle">
                     Tinder News
                 </h2>
