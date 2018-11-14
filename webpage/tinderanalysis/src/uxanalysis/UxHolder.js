83fb4196b9d606673a07021df5251f89ab295acb
estebandalelr <estebandalelr@gmail.com>
2018-11-13 19:29:55 -0500

    Visual improvements

--- webpage/tinderanalysis/src/uxanalysis/UxHolder.js
+++ webpage/tinderanalysis/src/uxanalysis/UxHolder.js
@@ -3,41 +3,32 @@ import React, { Component } from 'react'
 import './UxHolder.css'
 
 class UxHolder extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {}
-  }
-  render () {
-    return (
-      <div>
-        <h1 stripes='Striped Text'>
-          <span stripes='Striped Text'>
-                    Striped Text
-          </span>
-        </h1>
-        {/*                 <svg class="main-logo" viewBox="0 0 406 39">
-                    <title>
-                        Swipe Life
-                        </title>
-                    <defs>
-                        <path d="M1 31.4l6.5-7.8c2.8 3.3 8 5.6 13 5.6 2.2 0 3-1 3-1.9 0-1.3-1.7-2.2-4.6-3.1-5.3-1.7-13-4.4-13-12.2C6 7 11.2 0 21.8 0 27.8 0 34 2.3 38 6.4L31.4 14c-2.8-3-7.7-4.6-11-4.6-2 0-2.8 1-2.8 2 0 1.2 2 2 4.7 3 5.3 1.6 13 4.4 13 11.6 0 6.9-6 12.7-15.6 12.7-8 0-15-2.9-18.7-7.3zM75.3 38L75 16.8 65.6 38H53.8L51.3.6h12.4l-.2 22.9L74.2.6h8.5l.6 22.9L93 .6h13L87 38H75.3zm35.9 0L119.4.6h11.3L122.5 38h-11.3zm28.4 0L147.8.6h18.7c7.3 0 12 5 12 11 0 5.5-3.7 14.9-16.6 14.9h-8.5L150.9 38h-11.3zm22.6-21.4c2.9 0 4.6-1.5 4.6-3.7 0-1.8-1.2-2.8-3.4-2.8H157l-1.4 6.5h6.6zM187.6 38L196 .6h28.3l-2 9.5H205l-1 4.3h16.7l-2.1 9.4h-16.6l-1 4.8h17L216 38h-28.4zm69.7 0L265.5.6h11.3l-6 27.5h14.1l-2.1 9.9h-25.5zm40.6 0L306.1.6h11.3L309.2 38H298zm28 0L334 .6h28.3l-2 9.5h-17l-1 4.3H359l-2 9.4h-16.7L337.2 38h-11.3zm43.7 0L377.8.6h28.4l-2.1 9.5h-17l-1 4.3h16.7l-2.1 9.4H384l-1.1 4.8h17l-2 9.4h-28.4z" id="a-46">
-                        </path>
-                    </defs>
-                    <g transform="translate(-1)" fill="none" fill-rule="evenodd">
-                        <mask id="b-46" fill="#fff">
-                            <use href="#a-46">
-                            </use>
-                        </mask>
-                        <use fill="#000" href="#a-46">
-                        </use>
-                        <g mask="url(#b-46)" fill="#fff" opacity=".5">
-                            <path d="M-11 35h431v1.3H-11zM-11 32h431v1.3H-11zM-11 29h431v1.3H-11zM-11 26h431v1.3H-11zM-11 23h431v1.3H-11zM-11 20h431v1.3H-11zM-11 17h431v1.3H-11zM-11 14h431v1.3H-11zM-11 11h431v1.3H-11zM-11 8h431v1.3H-11zM-11 5h431v1.3H-11zM-11 2h431v1.3H-11z">
-                            </path>
-                        </g>
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+    render() {
+        return (
+            <div className="UX">
+                <svg width="60%"  viewBox="0 0 650 100">
+                    <desc>Section Title UX</desc>
+                    <g font-size="80" fill="url(#img2)" stroke="white" stroke-width="1" >
+                        <text x="1" class="text" y="90" font-style = "italic">About Tinder's UX</text>
+                        <defs>
+                            <pattern id="img2" patternUnits="userSpaceOnUse" width="100" height="100">
+                                <image 
+                                href="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.xmple.com%2Fwallpaper%2Fgrey-black-stripes-lines-streaks-1920x1080-c2-000000-dcdcdc-l2-91-91-a-0-f-1.svg&f=1" 
+                                x="0" 
+                                y="0" 
+                                width="830" 
+                                height="100" />
+                            </pattern>
+
+                        </defs>
                     </g>
-                </svg > */}
-      </div >)
-  }
+                </svg>
+            </div >)
+    }
 }
 
 export default UxHolder
