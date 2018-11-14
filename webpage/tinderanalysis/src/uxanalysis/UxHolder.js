686b90cb3edc9fc02341c6058fa6016ab83a0875
estebandalelr <estebandalelr@gmail.com>
2018-11-13 19:32:01 -0500

    Make title component

--- webpage/tinderanalysis/src/uxanalysis/UxHolder.js
+++ webpage/tinderanalysis/src/uxanalysis/UxHolder.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 import './UxHolder.css'
+import Title from './Title';
 
 class UxHolder extends Component {
     constructor(props) {
@@ -10,23 +11,7 @@ class UxHolder extends Component {
     render() {
         return (
             <div className="UX">
-                <svg width="60%"  viewBox="0 0 650 100">
-                    <desc>Section Title UX</desc>
-                    <g font-size="80" fill="url(#img2)" stroke="white" stroke-width="1" >
-                        <text x="1" class="text" y="90" font-style = "italic">About Tinder's UX</text>
-                        <defs>
-                            <pattern id="img2" patternUnits="userSpaceOnUse" width="100" height="100">
-                                <image 
-                                href="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.xmple.com%2Fwallpaper%2Fgrey-black-stripes-lines-streaks-1920x1080-c2-000000-dcdcdc-l2-91-91-a-0-f-1.svg&f=1" 
-                                x="0" 
-                                y="0" 
-                                width="830" 
-                                height="100" />
-                            </pattern>
-
-                        </defs>
-                    </g>
-                </svg>
+                <Title/>
             </div >)
     }
 }
