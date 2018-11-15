e829a0f92024981251fd1d97e797c34462f55f96
estebandalelr <estebandalelr@gmail.com>
2018-11-15 14:31:49 -0500

    Add data sources

--- webpage/tinderanalysis/src/apkanalysis/ApkHolder.js
+++ webpage/tinderanalysis/src/apkanalysis/ApkHolder.js
@@ -9,6 +9,7 @@ class ApkHolder extends Component {
         <h2 className='SectionTitle'>
           APK Analysis
         </h2>
+        Info taken from https://tinder.en.uptodown.com/android/old, http://downloadapk.net/Tinder.html and https://play.google.com/store/apps/details?id=com.tinder&hl=en
       </div>
     )
   }
