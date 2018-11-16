9e7c7130966d643e0ff180bee3b5b1758f34499d
estebandalelr <estebandalelr@gmail.com>
2018-11-15 22:38:48 -0500

    Add title, logo, image, move stuff from file to file

--- webpage/tinderanalysis/src/apkanalysis/ApkHolder.js
+++ webpage/tinderanalysis/src/apkanalysis/ApkHolder.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react'
 
 import './ApkHolder.css'
+
 import Chart from './Chart';
+import Logo from "./Logo";
 
 class ApkHolder extends Component {
   render() {
     return (
       <div>
-        <h2 className='SectionTitle'>
+        <h2 className='APKTitle'>
           APK Analysis
+          <Logo />
         </h2>
         <Chart />
-        <p>
-          Info taken from https://www.androidapksbox.com/tinder/, https://tinder.en.uptodown.com/android/old, http://downloadapk.net/Tinder.html and https://play.google.com/store/apps/details?id=com.tinder&hl=en
-        </p>
       </div>
     )
   }
--- webpage/tinderanalysis/src/apkanalysis/Chart.js
+++ webpage/tinderanalysis/src/apkanalysis/Chart.js
@@ -6,12 +6,30 @@ class Chart extends Component {
 
         render() {
                 return (
-                        <img
-                                className="chart"
-                                src={chart}
-                                width="80%"
-                        >
-                        </img>
+                        <div>
+                                <h2 className="title">
+                                APK size over time
+                                </h2>
+                                <img
+                                        className="chart"
+                                        src={chart}
+                                        width="80%"
+                                >
+                                </img>
+                                <p className="caption">
+                                        Info taken from <a href="https://www.androidapksbox.com/tinder/">
+                                                androidapksbox
+          </a>
+                                        , <a href="https://tinder.en.uptodown.com/android/old">
+                                                uptodown
+          </a>
+                                        , <a href="http://downloadapk.net/Tinder.html">
+                                                downloadapk
+          </a> and <a href="https://play.google.com/store/apps/details?id=com.tinder&hl=en">
+                                                the Play Store
+           </a>.
+        </p>
+                        </div>
                 )
         }
 }
