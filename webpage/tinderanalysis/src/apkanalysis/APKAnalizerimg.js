59df9c69b7a8e0de55d513563a284ac69dd19a2c
estebandalelr <estebandalelr@gmail.com>
2018-11-17 19:02:04 -0500

    Add images for versions

---
+++ webpage/tinderanalysis/src/apkanalysis/APKAnalizerimg.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class APKAnalyzerimg extends Component {
+    state = {}
+    render() {
+        return (
+            <div>
+
+                <img
+                    src={require("./apkanalyzer/" + this.props.version + ".png")}
+                    width="100%"
+                />
+                <p className="caption">
+                    Android Studio's  APK analyzer for {this.props.version}. This version has a total size of {this.props.size}MB.
+                </p>
+            </div>
+        );
+    }
+}
+
+export default APKAnalyzerimg;
--- webpage/tinderanalysis/src/apkanalysis/APKSize.js
+++ webpage/tinderanalysis/src/apkanalysis/APKSize.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 
 import Chart from './Chart';
+import APKAnalyzerimg from './APKAnalizerimg';
 
 class APKSize extends Component {
-    state = {}
-    render() {
+    render () {
         return (
             <div className="APKSize">
                 <Chart />
                 <p className="apktext">
                 The apk size has had constant growth over time. Tinder, a 5 year old app has seen a lot of the verionchanges to bundling and the increase in phone capabilities.
                 </p>
+                <APKAnalyzerimg size="67.4" version={"v9.4.1"}/>
+                <APKAnalyzerimg size="68" version={"v9.7.0"}/>
+                <APKAnalyzerimg size="69.2" version={"v9.11.1"}/>
+                <APKAnalyzerimg size="43" version={'v10.0.1'}/>
             </div>
         );
     }
