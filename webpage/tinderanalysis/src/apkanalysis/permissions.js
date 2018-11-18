42a9e46114474688f51536555cf408a6d31f0b77
Juan Guzmán <jdguzmans@hotmail.com>
2018-11-17 19:57:41 -0500

    permissions and multi threading done

--- webpage/tinderanalysis/src/apkanalysis/permissions.js
+++ webpage/tinderanalysis/src/apkanalysis/permissions.js
@@ -5,17 +5,34 @@ class Chart extends Component {
     return (
       <div>
         <h2 className='title'>
-          Permissions
+          Permissions (only shown relevant)
         </h2>
+
+        <div>
+          <h3>ACCESS_NETWORK_STATE</h3>
+          <p>It is widelly used to gather information about the connectivity. For example, this class,
+            which uses the ReactiveNetwork library for bindings uses this permission.
+          </p>
+          <img src='resources/permissions4.png' />
+        </div>
+
         <div>
           <h3>READ_EXTERNAL_STORAGE</h3>
           <p>
-            This permission is used in the activity that edits the profile of an user, specifically when a profile picture is edited.
-          </p>
-          <p>If this permission is denied an explanation is shown to the user.
+            It is used in the activity that edits the profile of an user, specifically when a profile picture is edited.
           </p>
           <img src='resources/permissions0.png' />
+          <p>
+            It is also used in when reading application info, metadata and storage keys.
+          </p>
+          <img src='resources/permissions5.png' />
         </div>
+
+        <div>
+          <h3>WRITE_EXTERNAL_STORAGE</h3>
+          <p>It is used when writing application info, like metadata and storage keys</p>
+        </div>
+
         <div>
           <h3>ACCESS_COARSE_LOCATION</h3>
           <p>
@@ -27,6 +44,24 @@ class Chart extends Component {
           </p>
           <img src='resources/permissions1.png' />
         </div>
+
+        <div>
+          <h3>VIBRATE</h3>
+          <p>When a user has a match there is a vibration. This an other cases use the permission to vibrare.
+            In the following section of code there is a vibration, unfortunattely, it is very difficult to understand what the
+            code is actually for.
+          </p>
+          <img src='resources/permissions3.png' />
+        </div>
+
+        <div>
+          <h3>BILLING</h3>
+          <p>
+            Users can do purchases like subscribing to tinder gold.
+          </p>
+          <img src='resources/permissions2.png' />
+        </div>
+
       </div>
     )
   }
