28e8e1812c5511e2260f0e199c5a88b966cf0b84
estebandalelr <estebandalelr@gmail.com>
2018-11-18 21:42:32 -0500

    Add classnames for style

--- webpage/tinderanalysis/src/apkanalysis/permissions.js
+++ webpage/tinderanalysis/src/apkanalysis/permissions.js
@@ -10,56 +10,56 @@ class Chart extends Component {
 
         <div>
           <h3>ACCESS_NETWORK_STATE</h3>
-          <p>It is widelly used to gather information about the connectivity. For example, this class,
+          <p className='apktext'>It is widelly used to gather information about the connectivity. For example, this class,
             which uses the ReactiveNetwork library for bindings uses this permission.
           </p>
-          <img src='resources/permissions4.png' alt='code snippet' />
+          <img src='resources/permissions4.png' alt='code snippet' width='90%' />
         </div>
 
         <div>
           <h3>READ_EXTERNAL_STORAGE</h3>
-          <p>
+          <p className='apktext'>
             It is used in the activity that edits the profile of an user, specifically when a profile picture is edited.
           </p>
-          <img src='resources/permissions0.png' alt='code snippet' />
-          <p>
+          <img src='resources/permissions0.png' alt='code snippet' width='90%' />
+          <p className='apktext'>
             It is also used in when reading application info, metadata and storage keys.
           </p>
-          <img src='resources/permissions5.png' alt='code snippet' />
+          <img src='resources/permissions5.png' alt='code snippet' width='90%' />
         </div>
 
         <div>
           <h3>WRITE_EXTERNAL_STORAGE</h3>
-          <p>It is used when writing application info, like metadata and storage keys</p>
+          <p className='apktext'>It is used when writing application info, like metadata and storage keys</p>
         </div>
 
         <div>
           <h3>ACCESS_COARSE_LOCATION</h3>
-          <p>
+          <p className='apktext'>
             It is used constantly in the app in order to request the location of the user
             for displaying people close to him.
           </p>
-          <p>
+          <p className='apktext'>
             We can see how it is requested in the lifecycle method onResume in the MainActivity.
           </p>
-          <img src='resources/permissions1.png' alt='code snippet' />
+          <img src='resources/permissions1.png' alt='code snippet' width='90%' />
         </div>
 
         <div>
           <h3>VIBRATE</h3>
-          <p>When a user has a match there is a vibration. This an other cases use the permission to vibrare.
+          <p className='apktext'>When a user has a match there is a vibration. This an other cases use the permission to vibrare.
             In the following section of code there is a vibration, unfortunattely, it is very difficult to understand what the
             code is actually for.
           </p>
-          <img src='resources/permissions3.png' alt='code snippet' />
+          <img src='resources/permissions3.png' alt='code snippet' width='90%' />
         </div>
 
         <div>
           <h3>BILLING</h3>
-          <p>
+          <p className='apktext'>
             Users can do purchases like subscribing to tinder gold.
           </p>
-          <img src='resources/permissions2.png' alt='Tinder billing screen' />
+          <img src='resources/permissions2.png' alt='Tinder billing screen' width='90%' />
         </div>
 
       </div>
