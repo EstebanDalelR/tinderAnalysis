ba02463d98d879cad3992776dfa2a70fdac6cf8b
estebandalelr <estebandalelr@gmail.com>
2018-11-18 21:38:36 -0500

    Change image width

--- webpage/tinderanalysis/src/apkanalysis/persistence.js
+++ webpage/tinderanalysis/src/apkanalysis/persistence.js
@@ -9,11 +9,15 @@ class Chart extends Component {
         </h2>
         <div>
           <h3>SQLite</h3>
-          <img src='resources/persistence0.png' alt='code snippet' />
+          <img width='90%'
+            src='resources/persistence0.png'
+            alt='code snippet' />
           <p>
             To handle local persistence, tinder uses SQLite, with the pattern of facade.
           </p>
-          <img src='resources/persistence1.png' alt='code snippet' />
+          <img width='90%'
+            src='resources/persistence1.png'
+            alt='code snippet' />
           <p>
             They create a class 'C2714a', which exposes all the methods that handle local persistence.
           </p>
